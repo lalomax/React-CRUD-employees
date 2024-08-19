97b11103ea5ed4b6456bd0a43775ad3d6534dc88
Orlando <lalomax365@gmail.com>
2024-08-19 19:36:58 +0000

    style: naming conventions
    Use consistent naming conventions. For instance, "Add Employee" could be "Add New Employee". closed #4

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -31,7 +31,7 @@ export const Heading = () => {
               <line x1="12" y1="8" x2="12" y2="16"></line>
               <line x1="8" y1="12" x2="16" y2="12"></line>
             </svg>
-            <span className="pl-2">Add Employee</span>
+            <span className="pl-2">Add New Employee</span>
           </button>
         </div>
       </div>
